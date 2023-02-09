e910859873ea61b5a47bd659544e6be4e3ed9d75
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2023-02-09 15:22:05 -0800

    fix namespace collision to run basic_test script

--- python/basic_test.py
+++ python/basic_test.py
@@ -3,7 +3,7 @@ print("starting basic_test.py")
 # test 1: with our color matrix from assets, try replacing noise-y black elements with white elements
 # keep both onhand and do a side by side comparison with pyplot
 from color_matrix import get_color_matrix
-from clean_noise import clean_noise
+from clean_noise import *
 import matplotlib.pyplot as plt
 import json
   
@@ -16,7 +16,7 @@ data = json.load(f)
 # # print(data.keys())
 mapdata = data["mapdata"]
 color_matrix = get_color_matrix(mapdata)
-updated_color_matrix = clean_noise(color_matrix, 0, 2, 1, 0.5)
+updated_color_matrix = clean_color_matrix(color_matrix, 0, 2, 1, 0.5)
 
 # print("starting plot")
 # # updated_color_matrix = plt.imclose(color_matrix)
