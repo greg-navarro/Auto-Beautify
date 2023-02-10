afe2457a60dbb1b4301de57eb81d62ce1e97ce7b
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2023-02-09 16:11:07 -0800

    rewrites basic_test script: adds section for constants and saves/shows resultant side by side image

--- python/basic_test.py
+++ python/basic_test.py
@@ -1,38 +1,47 @@
 print("starting basic_test.py")
 
-# test 1: with our color matrix from assets, try replacing noise-y black elements with white elements
-# keep both onhand and do a side by side comparison with pyplot
+# test 1: with our color matrix from assets, try replacing noise-y black elements with white elements and do a side by side comparison with pyplot
 from color_matrix import get_color_matrix
 from clean_noise import *
 import matplotlib.pyplot as plt
 import json
+import os
+
+# Constants relevant to processing algo
+THRESHOLD = 1
+POSITIVITY_RATIO = 0.2
+WALL = 0
+GROUND = 1
+# TODO: no definite value for UNDEFINED in this implementation
+#       this can be corrected in color_matrix.py
+TARGET_TYPE = WALL
+REPLACEMENT_TYPE = GROUND
+
   
 # Opening JSON file
 f = open('/Users/administrator/Documents/GitHub/parse-.umap/assets/map.umap')
-  
-# returns JSON object as 
-# a dictionary
 data = json.load(f)
-# # print(data.keys())
 mapdata = data["mapdata"]
+# process umap data
 color_matrix = get_color_matrix(mapdata)
-updated_color_matrix = clean_color_matrix(color_matrix, 0, 2, 1, 0.5)
+# run processing algos (that this script is currently testing)
+updated_color_matrix = clean_color_matrix(color_matrix, TARGET_TYPE, REPLACEMENT_TYPE, THRESHOLD, POSITIVITY_RATIO)
+# visually plot output
+print("starting plot")
+# updated_color_matrix = plt.imclose(color_matrix)
+plt.figure(figsize=(8,8))
+plt.subplot(121)
 
-# print("starting plot")
-# # updated_color_matrix = plt.imclose(color_matrix)
-# plt.figure(figsize=(8,8))
-# plt.subplot(121)
+plt.imshow(color_matrix) #, cmap="Greys")
+plt.axis('off')
+plt.title('unprocessed')
 
-# plt.imshow(color_matrix) #, cmap="Greys")
-# plt.axis('off')
-# plt.title('unprocessed')
+plt.subplot(122)
 
-# plt.subplot(122)
-
-# plt.imshow(updated_color_matrix) #, cmap="Greys")
-# plt.axis('off')
-# plt.title('processed with clean_noise()')
-
-# plt.show()
+plt.imshow(updated_color_matrix) #, cmap="Greys")
+plt.axis('off')
+plt.title('threshold: {} -- percent positivity {}'.format(THRESHOLD, POSITIVITY_RATIO))
+plt.savefig('/Users/administrator/Documents/GitHub/Auto-Beautify/test-results/imageth{}pos{}.png'.format(THRESHOLD, POSITIVITY_RATIO))
+plt.show()
 
 print("ending basic_test.py")
